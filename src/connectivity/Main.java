b60533e47d18025b680cfbc9f907fabc37e3a50f
KharYusuf <44437040+KharYusuf@users.noreply.github.com>
2019-07-23 19:04:48 +0530

    Add files via upload

---
+++ src/connectivity/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package connectivity;
+
+/**
+ *
+ * @author burhanudin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
---
+++ src/connectivity/nc.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package connectivity;
+
+/**
+ *
+ * @author Welcome
+ */
+public class nc {
+
+}
